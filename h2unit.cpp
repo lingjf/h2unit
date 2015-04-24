67170c650ae5a1bad910b2531389a57310ca0098
lingjf <lingjf@gmail.com>
2015-04-24 20:18:03 +0800

    support hex number in JSON comparation

--- h2unit.cpp
+++ h2unit.cpp
@@ -680,8 +680,16 @@ static const char *parse_number(cJSON *item, const char *num)
   double n = 0, sign = 1, scale = 0; int subscale = 0, signsubscale = 1;
 
   if (*num == '-') sign = -1, num++;  /* Has sign? */
-  if (*num == '0') num++;     /* is zero */
-    if (*num >= '1' && *num <= '9') do  n = (n * 10.0) + (*num++ -'0'); while (*num >= '0' && *num <= '9'); /* Number? */
+  if (*num == '0') { num++;
+    if (*num == 'x' || *num == 'X') { /* Hex? */
+      for (num++; (*num >= '0' && *num <= '9') || (*num >= 'A' && *num <= 'F') || (*num >= 'a' && *num <= 'f'); num++) {
+        if (*num >= '0' && *num <= '9') n = (n * 16) + (*num) - '0';
+        else if (*num >= 'A' && *num <= 'F') n = (n * 16) + 10 + (*num) - 'A';
+        else if (*num >= 'a' && *num <= 'f') n = (n * 16) + 10 + (*num) - 'a';
+      }
+    }
+  }
+  if (*num >= '1' && *num <= '9') do  n = (n * 10.0) + (*num++ -'0'); while (*num >= '0' && *num <= '9'); /* Number? */
   if (*num == '.' && num[1] >= '0' && num[1] <= '9') {num++;    do  n = (n * 10.0) + (*num++ -'0'), scale--; while (*num >= '0' && *num <= '9');} /* Fractional part? */
   if (*num == 'e' || *num == 'E') { /* Exponent? */
     num++; if (*num == '+') num++;  else if (*num == '-') signsubscale = -1, num++;   /* With sign? */
